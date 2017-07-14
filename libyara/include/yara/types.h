867c46151e92f36990540d4f6e6bef5e2765143a
Rastislav Masaryk <rmasaryk@eset.sk>
2017-07-14 14:37:11 +0200

    merge master

--- libyara/include/yara/types.h
+++ libyara/include/yara/types.h
@@ -70,11 +70,8 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define STRING_GFLAGS_CHAIN_TAIL        0x4000
 #define STRING_GFLAGS_FIXED_OFFSET      0x8000
 #define STRING_GFLAGS_GREEDY_REGEXP     0x10000
-<<<<<<< HEAD
 #define STRING_GFLAGS_LEET              0x20000
-=======
-#define STRING_GFLAGS_DOT_ALL           0x20000
->>>>>>> origin/master
+#define STRING_GFLAGS_DOT_ALL           0x40000
 
 #define STRING_IS_HEX(x) \
     (((x)->g_flags) & STRING_GFLAGS_HEXADECIMAL)
